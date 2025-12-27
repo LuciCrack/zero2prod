3e3f3cc1415766a98437ec19835ea39ec93ccdc6
lupici <lucianapardorojas@gmail.com>
2025-12-26 22:38:49 -0300

    Avoid Unused warning

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,5 @@ use zero2prod::run;
 #[tokio::main]
 async fn main() {
     let listener = tokio::net::TcpListener::bind("0.0.0.0:2000").await.unwrap();
-    run(listener).await;
+    let _ = run(listener).await;
 }
