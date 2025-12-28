95f46acbe0c30a8617b2c76233e5c5d578c0054d
lupici <lucianapardorojas@gmail.com>
2025-12-28 18:38:16 -0300

    feat: actually save user data on subscription

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,4 +1,9 @@
 use axum::Form;
+use axum::extract::State;
+use axum::http::StatusCode;
+use chrono::Utc;
+use sqlx::PgPool;
+use uuid::Uuid;
 
 #[derive(serde::Deserialize)]
 pub struct FormData {
@@ -6,4 +11,20 @@ pub struct FormData {
     email: String,
 }
 
-pub async fn subscribe(Form(data): Form<FormData>) {}
+pub async fn subscribe(State(pool): State<PgPool>, Form(data): Form<FormData>) -> StatusCode {
+    let result = sqlx::query!(
+        r#"INSERT INTO subscriptions (id, email, name, subscribed_at) VALUES ($1, $2, $3, $4)"#,
+        Uuid::new_v4(),
+        data.email,
+        data.name,
+        Utc::now()
+    ).execute(&pool).await;
+
+    match result {
+        Ok(_) => StatusCode::OK,
+        Err(e) => {
+            tracing::error!("Error in subscribing query: {e}");
+            StatusCode::INTERNAL_SERVER_ERROR
+        },
+    }
+}
