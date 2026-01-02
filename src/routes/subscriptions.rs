5266fe79194a528f73417c83a6414d9d25ca1e7d
lupici <lucianapardorojas@gmail.com>
2026-01-02 15:27:10 -0300

    implement tracing instrument

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -11,22 +11,42 @@ pub struct FormData {
     email: String,
 }
 
+#[tracing::instrument(
+    name = "Adding a new subscriber",
+    skip_all,
+    fields(
+        request_id=%Uuid::new_v4(),
+        subscriber_email=%data.email,
+        subscriber_name=%data.name,
+    )
+)]
 pub async fn subscribe(State(pool): State<PgPool>, Form(data): Form<FormData>) -> StatusCode {
-    let result = sqlx::query!(
+    match insert_subscriber(&pool, &data).await {
+        Ok(_) => StatusCode::OK,
+        Err(e) => {
+            tracing::error!("Error in subscribing query: {:?}", e);
+            StatusCode::INTERNAL_SERVER_ERROR
+        }
+    }
+}
+
+#[tracing::instrument(
+    name = "Saving new subscriber to database"
+    skip_all
+)]
+async fn insert_subscriber(pool: &PgPool, data: &FormData) -> Result<(), sqlx::Error> {
+    sqlx::query!(
         r#"INSERT INTO subscriptions (id, email, name, subscribed_at) VALUES ($1, $2, $3, $4)"#,
         Uuid::new_v4(),
         data.email,
         data.name,
         Utc::now()
     )
-    .execute(&pool)
-    .await;
-
-    match result {
-        Ok(_) => StatusCode::OK,
-        Err(e) => {
-            tracing::error!("Error in subscribing query: {e}");
-            StatusCode::INTERNAL_SERVER_ERROR
-        }
-    }
+    .execute(pool)
+    .await
+    .map_err(|e| {
+        tracing::error!("Error in inserting subscription: {:?}", e);
+        e
+    })?;
+    Ok(())
 }
