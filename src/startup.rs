024b3f7ed4e279b6e3eff979ee80c101134509c4
lupici <lucianapardorojas@gmail.com>
2026-01-02 22:32:42 -0300

    Add middleware for using request_id in tracing subscriber

--- src/startup.rs
+++ src/startup.rs
@@ -2,17 +2,18 @@ use axum::Router;
 use axum::routing::{get, post};
 use sqlx::PgPool;
 use tokio::net::TcpListener;
-use tower_http::trace::TraceLayer;
 
 use crate::routes::{health_check, subscribe};
+use crate::telemetry::create_subscriber_middleware_layer;
 
 pub async fn run(listener: TcpListener, pool: PgPool) -> Result<(), std::io::Error> {
+    let middleware_layer = create_subscriber_middleware_layer();
     // Build application
     let app = Router::new()
         .route("/health_check", get(health_check))
         .route("/subscriptions", post(subscribe))
         .with_state(pool)
-        .layer(TraceLayer::new_for_http());
+        .layer(middleware_layer);
 
     // run our app with hyper, listening globally on port 2000
     tracing::info!("App running on http://{}", listener.local_addr()?);
--- src/telemetry.rs
+++ src/telemetry.rs
@@ -1,10 +1,20 @@
-use tracing::Subscriber;
+use axum::body::Body;
 use tracing::subscriber::set_global_default;
+use tracing::{Span, Subscriber};
 use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
 use tracing_log::LogTracer;
 use tracing_subscriber::fmt::MakeWriter;
 use tracing_subscriber::layer::SubscriberExt;
 use tracing_subscriber::{EnvFilter, Registry};
+
+use axum::http::{HeaderName, Request};
+use tower::ServiceBuilder;
+use tower::layer::util::Stack;
+use tower_http::classify::{ServerErrorsAsFailures, SharedClassifier};
+use tower_http::request_id::{MakeRequestId, RequestId, SetRequestIdLayer};
+use tower_http::trace::TraceLayer;
+use tower_layer::Identity;
+use uuid::Uuid;
 
 /// Compose multiple layers into a `tracing`'s subscriber.
 ///
@@ -39,4 +49,47 @@ where
 pub fn init_subscriber(subscriber: impl Subscriber + Send + Sync) {
     LogTracer::init().expect("Failed to set logger");
     set_global_default(subscriber).expect("Failed to set subscriber");
+}
+
+/// Create a tower_http Tracer Layer for the application
+pub fn create_subscriber_middleware_layer() -> ServiceBuilder<
+    Stack<
+        // Note the exact types here:
+        TraceLayer<SharedClassifier<ServerErrorsAsFailures>, fn(&Request<Body>) -> Span>,
+        Stack<SetRequestIdLayer<MyMakeRequestId>, Identity>,
+    >,
+> {
+    let x_request_id = HeaderName::from_static("x-request-id");
+
+    ServiceBuilder::new()
+        .layer(SetRequestIdLayer::new(x_request_id, MyMakeRequestId))
+        .layer(
+            // Pass the function pointer here instead of a closure
+            TraceLayer::new_for_http().make_span_with(make_span_with_request_id),
+        )
+}
+
+#[derive(Clone, Copy)]
+pub struct MyMakeRequestId;
+
+impl MakeRequestId for MyMakeRequestId {
+    fn make_request_id<B>(&mut self, _request: &Request<B>) -> Option<RequestId> {
+        let request_id = Uuid::new_v4().to_string().parse().unwrap();
+        Some(RequestId::new(request_id))
+    }
+}
+
+fn make_span_with_request_id(request: &Request<Body>) -> Span {
+    let request_id = request
+        .extensions()
+        .get::<RequestId>()
+        .map(|id| id.header_value().to_str().unwrap_or(""))
+        .unwrap_or("");
+
+    tracing::info_span!(
+        "http_request",
+        %request_id,
+        method = %request.method(),
+        uri = %request.uri(),
+    )
 }
